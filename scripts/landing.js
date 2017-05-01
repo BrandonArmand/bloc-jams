0b98ac1a0180bb79b57f3cad4c33610d480323a4
brandon welsh <brandon welsh>
2017-04-30 22:12:51 -0700

    checkpoint 9 assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,17 +4,9 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
 
-    function revealPoint(){
-        for(var i = 0; i < points.length; i++){
-          points[i].style.opacity = 1;
-          points[i].style.transform = "scaleX(1) translateY(0)";
-          points[i].style.msTransform = "scaleX(1) translateY(0)";
-          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
+  forEach(points);
 
-    };
-
-    revealPoint();
+  revealPoint();
 };
 
 window.onload = function(){
---
+++ scripts/utilities.js
@@ -0,0 +1,9 @@
+function forEach(array){
+  for(var i = 0; i < array.length; i++){
+    array[i].style.opacity = 1;
+    array[i].style.transform = "scaleX(1) translateY(0)";
+    array[i].style.msTransform = "scaleX(1) translateY(0)";
+    array[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+
+}
