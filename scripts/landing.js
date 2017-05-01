4f1051d5dd9e805355ab7493943b2b976f5fc1e0
brandon welsh <brandon welsh>
2017-04-30 20:57:19 -0700

    checkpoint 9 events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,8 @@
-var animatePoints = function() {
 
-    var points = document.getElementsByClassName('point');
+
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
 
     function revealPoint(){
         for(var i = 0; i < points.length; i++){
@@ -14,3 +16,18 @@ var animatePoints = function() {
 
     revealPoint();
 };
+
+window.onload = function(){
+  if(window.innerHeight > 950){
+    animatePoints(pointsArray);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event){
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+      animatePoints(pointsArray);
+    }
+  })
+}
